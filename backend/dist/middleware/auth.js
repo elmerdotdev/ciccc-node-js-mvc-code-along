5f982da707de16b0c99983ff7adb3029182eade5
Elmer Balbin <contact@elmer.dev>
2024-11-06 17:11:14 -0800

    updated middleware

--- backend/dist/middleware/auth.js
+++ backend/dist/middleware/auth.js
@@ -3,12 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.cookieAuthCheck = void 0;
 const cookieAuthCheck = (req, res, next) => {
     const { isAuthenticated } = req.signedCookies;
-    console.log(req.cookies);
     if (isAuthenticated) {
         next();
     }
     else {
-        res.status(403).send('Middleware');
+        res.status(403).send();
     }
 };
 exports.cookieAuthCheck = cookieAuthCheck;
--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -2,10 +2,9 @@ import { Request, Response, NextFunction } from 'express'
 
 export const cookieAuthCheck = (req: Request, res: Response, next: NextFunction) => {
   const { isAuthenticated } = req.signedCookies
-  console.log(req.cookies)
   if (isAuthenticated) {
     next()
   } else {
-    res.status(403).send('Middleware')
+    res.status(403).send()
   }
 }
